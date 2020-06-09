68881530096daa94a8aca033ea442fa5e1ad434b
matky <tamk0584@gmail.com>
2020-06-09 07:45:11 +0000

    add status column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,6 +45,7 @@ class TasksController extends Controller
     {
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect('/');
@@ -91,6 +92,7 @@ class TasksController extends Controller
     {
         $task = Task::findOrFail($id);
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect('/');
